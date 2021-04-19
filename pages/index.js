169d6177d7f922c6ff4bf332bdeeec96210755d5
AYMEN <moniakamli95@gmail.com>
2021-04-19 12:55:45 +0200

    building the avatar component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Avatar from '../components/Avatar'
 
 export default function Home() {
   return (
@@ -16,7 +17,9 @@ export default function Home() {
         <div className="flex space-x-4 items-center">
           <p className="link">Gmail</p>
           <p className="link">Images</p>
+        <Avatar url="https://media-exp1.licdn.com/dms/image/C4D35AQH4tXKFfESRwA/profile-framedphoto-shrink_200_200/0/1613494608179?e=1618912800&v=beta&t=9ZA0bofmk5d7hYRfeCRlpj7evaAS8gzg81L4Hu00cB8"/>
         </div>
+
       </header>
     </div>
   )
