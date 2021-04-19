200754c7b958d147a91374e735f656a576f4da54
AYMEN <moniakamli95@gmail.com>
2021-04-19 11:47:15 +0200

    building the front page

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,20 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Google App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Lets build Google YOOOO</h1>
+      <header className="flex w-full p-5 justify-between text-sm text-gray-700">
+        <div className="flex space-x-4 items-center">
+          <p className="link">About</p>
+          <p className="link">Store</p>
+        </div>
+
+        <div className="flex space-x-4 items-center">
+          <p className="link">Gmail</p>
+          <p className="link">Images</p>
+        </div>
+      </header>
     </div>
   )
 }
